96054aa7711299cb8e15466c0000eca4762b6886
bgenchel <benjiegenchel@gmail.com>
2015-05-11 22:04:31 -0700

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var app = require('../app');
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index', {});
+});
+
+module.exports = router;
